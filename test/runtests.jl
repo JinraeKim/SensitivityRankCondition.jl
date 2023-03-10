1974a6a068e93107c58d4f622968dbc82e6fddb2
JinraeKim <kjl950403@gmail.com>
2023-03-10 13:46:54 +0900

    Fix typoes

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using SERC
 using Test
 
 @testset "SERC.jl" begin
-    include("test/local_strong_accessibility.jl")
-    include("test/split_svdvals.jl")
+    include("local_strong_accessibility.jl")
+    include("split_svdvals.jl")
 end
